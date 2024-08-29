136e423fa2fd7f4d0e98fda2065991e1ad1533a8
a29cute <allsun0829@gmail.com>
2024-08-29 13:47:00 +0900

    [피드백 반영] 메뉴, 모달창 열릴 때 외부 스크롤 안되도록 처리, 외부 클릭했을 때 창닫힘

--- src/js/modal-handler.js
+++ src/js/modal-handler.js
@@ -1,17 +1,25 @@
-// 모달을 표시하는 함수
 function showModal(event) {
-    event.preventDefault(); // 기본 동작을 방지
-    if (inputCheck()) { // 입력값이 유효한 경우
-        document.getElementById('modal').style.display = 'flex'; // 모달 표시
+    event.preventDefault();
+    if (inputCheck()) {
+        document.getElementById('modal').style.display = 'flex';
+        document.body.style.overflow = 'hidden'; // 스크롤 방지
     }
 }
 
-// 모달을 닫는 함수
+
 function closeModal(event) {
-    event.preventDefault(); // 기본 동작을 방지
-    document.getElementById('modal').style.display = 'none'; // 모달 숨기기
+    event.preventDefault();
+    document.getElementById('modal').style.display = 'none';
+    document.body.style.overflow = '';
 }
 
-// 이벤트 리스너를 설정
+
 document.querySelector('.subscribeButton').addEventListener('click', showModal);
 document.querySelector('.modalButton').addEventListener('click', closeModal);
+
+window.addEventListener('click', function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target === modal) {
+        closeModal(event);
+    }
+});
--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -3,12 +3,28 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuCloseButton = document.querySelector('.menuCloseButton');
     const menu = document.querySelector('aside');
 
-    menuOpenButton.addEventListener('click', function() {
+
+    menuOpenButton.addEventListener('click', function(event) {
+        event.stopPropagation();
         menu.classList.add('open');
+        document.body.style.overflow = 'hidden';
     });
 
-    menuCloseButton.addEventListener('click', function() {
+
+    menuCloseButton.addEventListener('click', function(event) {
+        event.stopPropagation(); // 이벤트 전파 방지
         menu.classList.remove('open');
+        document.body.style.overflow = '';
     });
 
+    document.addEventListener('click', function(event) {
+        if (menu.classList.contains('open') && !menu.contains(event.target) && !menuOpenButton.contains(event.target)) {
+            menu.classList.remove('open');
+            document.body.style.overflow = '';
+        }
+    });
+
+    menu.addEventListener('click', function(event) {
+        event.stopPropagation(); // 이벤트 전파 방지
+    });
 });
